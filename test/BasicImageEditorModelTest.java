2be2a1c1e711f48d9d5c849601160b785aebdf7c
jekhi5 <jacobk513@gmail.com>
2022-06-07 13:12:30 -0400

    began testing BasicImageEditorModel

--- test/BasicImageEditorModelTest.java
+++ test/BasicImageEditorModelTest.java
@@ -1,3 +1,5 @@
+import org.junit.Before;
+
 import model.BasicImageEditorModel;
 import model.ImageEditorModel;
 import model.image.Image;
@@ -5,8 +7,24 @@ import utilities.ImageUtil;
 
 public class BasicImageEditorModelTest {
 
-  Image koalaRegPPM = ImageUtil.createImageFromPath("resources/Koala.ppm");
-  Image
-  ImageEditorModel model = new BasicImageEditorModel();
+  Image fullyRedPPM;
+  Image fullyGreenPPM;
+  Image fullyBluePPM;
+  Image checkeredBlackRight;
+  Image checkeredBlackBottom;
+  ImageEditorModel model;
+
+  @Before
+  public void init() {
+    fullyRedPPM = ImageUtil.createImageFromPath("resources/FullyRed_3x3.ppm");
+    fullyGreenPPM = ImageUtil.createImageFromPath("resources/FullyGreen_3x3.ppm");
+    fullyBluePPM = ImageUtil.createImageFromPath("resources/FullyBlue_3x3.ppm");
+    checkeredBlackRight = ImageUtil.createImageFromPath("resources/CheckeredBlackRight_3x4" +
+            ".ppm");
+    checkeredBlackBottom = ImageUtil.createImageFromPath("resources/CheckeredBlackBottom_3x4" +
+            ".ppm");
+    model = new BasicImageEditorModel();
+  }
+
 
 }
