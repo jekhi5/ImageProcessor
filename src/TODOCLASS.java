7a529f46706b529f10de92c5cf2af5c19ce4022b
jekhi5 <jacobk513@gmail.com>
2022-06-12 16:42:18 -0400

    made TODOCLASS

---
+++ src/TODOCLASS.java
@@ -0,0 +1,10 @@
+// TODO: Make fun name
+// TODO: "help" command
+// TODO: file-by-file cleanup (remove System.out.println(), check for leakage, remove warnings, etc)
+// TODO: run coverage on all tests
+// TODO: See how to handle the image-creation-time in the tests
+// TODO: Handle testCommand/debugCommand
+
+
+
+// TODO: DELETE THIS FILE
