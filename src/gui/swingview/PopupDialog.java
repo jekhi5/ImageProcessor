0c77b63411d0572f633919a8f5040ec4981ebfe7
jekhi5 <92763368+Jacob-Kline-NEU@users.noreply.github.com>
2022-06-20 16:56:42 -0400

    implemented the popup window with proper color and format

--- src/gui/swingview/PopupDialog.java
+++ src/gui/swingview/PopupDialog.java
@@ -1,51 +1,59 @@
 package gui.swingview;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Font;
 
-import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JTextPane;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
 /**
  * Represents a window that only shows a text message and a close button.
  */
 public class PopupDialog extends JFrame {
 
+  private static final int WIDTH = 300;
+  private static final int MAX_HEIGHT = 500;
+
   /**
    * Creates a new popup dialog.
+   *
    * @param msg the message to display
    */
   public PopupDialog(String msg) {
     super("ImageEditor");
-    this.setSize(400, 300);
-    this.setResizable(false);
-    this.setLocationRelativeTo(null);
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.getContentPane().setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
 
-    JLabel text = new JLabel(msg);
-    text.setSize(250, 200);
-    text.setAlignmentX(Component.CENTER_ALIGNMENT);
-    text.setBorder(BorderFactory.createTitledBorder("JLabel"));
+    JTextPane textPane = new JTextPane();
+    textPane.setEnabled(true);
+    textPane.setEditable(false);
 
-    JTextArea text2 = new JTextArea(msg);
-    text2.setLineWrap(true);
-    text2.setWrapStyleWord(true);
-    text2.setEditable(false);
+    textPane.setBackground(new Color(234, 234, 234));
+    textPane.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
+    textPane.setBorder(BorderFactory.createTitledBorder("Notice:"));
 
-    text2.setSize(15, 10);
-    text2.setAlignmentX(Component.CENTER_ALIGNMENT);
-    text2.setBorder(BorderFactory.createTitledBorder("JTextArea"));
+    StyledDocument styledDocument = textPane.getStyledDocument();
+    SimpleAttributeSet centeringAttribute = new SimpleAttributeSet();
+    StyleConstants.setAlignment(centeringAttribute, StyleConstants.ALIGN_CENTER);
+    styledDocument.setParagraphAttributes(0, styledDocument.getLength(), centeringAttribute, false);
+    
+    Style styledText = textPane.addStyle("", null);
+    StyleConstants.setForeground(styledText, new Color(109, 109, 109));
+    try {
+      styledDocument.insertString(0, msg, styledText);
+    } catch (javax.swing.text.BadLocationException e) {
+      textPane.setText(msg);
+    }
 
-    JTextPane text3 = new JTextPane();
-    text3.setText(msg);
-
-    text3.setEditable(false);
-    text3.setAlignmentX(Component.CENTER_ALIGNMENT);
-    text3.setBorder(BorderFactory.createTitledBorder("JTextPane"));
-
-    this.getContentPane().add(text);
-    this.getContentPane().add(text2);
-    this.getContentPane().add(text3);
-
+    int height = Math.min((int) (Math.ceil(textPane.getText().length() / (double) WIDTH)) * 112,
+            MAX_HEIGHT);
+    this.setSize(WIDTH, height);
+    this.add(textPane);
     this.setVisible(true);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setLocationRelativeTo(null);
   }
 }
--- src/gui/swingview/Tester.java
+++ src/gui/swingview/Tester.java
@@ -4,6 +4,10 @@ import javax.swing.*;
 
 public class Tester {
   public static void main(String[] args) {
-    JFrame popup = new PopupDialog("Hello World!");
+    JFrame popup = new PopupDialog("asjkdfhslkdjfha dlsfkjahsdf laksjdhf alsdkjfha lsdkfjh " +
+            "alsdkfjhasldkfjha lsdkjfh alsdkjfh laskdjfh laskdjfh laskdjfh laksdjfh alskdjfh " +
+            "alksdjfh alskjdfh laksdjhf alksjdfh alksjdfhalkjsdfh laksdjhf alskjdhf laskdjfh " +
+            "aklsdfjhaslkdjfha skdjfh alskdjfh laksdjfh lskdjfh lskdfjha lsdkjfah lskdfjha " +
+            "lskdfjh asldkjfh alskdjfh lksdjfha ldksjfha lksdjfh alskjdfh alksjdfh alskjdfh ");
   }
 }
