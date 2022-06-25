bb9afb62cf8693c32af6d47db907d6bbe8fefb36
jekhi5 <92763368+Jacob-Kline-NEU@users.noreply.github.com>
2022-06-25 10:57:28 -0400

    still working

--- src/gui/ColoredPopup.java
+++ src/gui/ColoredPopup.java
@@ -30,7 +30,6 @@ public class ColoredPopup extends JTextPane {
   static private final Color B_Green = Color.getHSBColor(0.333f, 1.000f, 1.000f);
   static private final Color B_Yellow = Color.getHSBColor(0.167f, 1.000f, 1.000f);
   static private final Color B_Cyan = Color.getHSBColor(0.500f, 1.000f, 1.000f);
-  static private final Color B_White = Color.getHSBColor(0.000f, 0.000f, 1.000f);
   static private final Color cReset = Color.getHSBColor(0.000f, 0.000f, 1.000f);
   static private Color colorCurrent = cReset;
   private String remaining = "";
@@ -50,10 +49,10 @@ public class ColoredPopup extends JTextPane {
 
   private void appendANSI(String s) { // convert ANSI color codes first
     int aPos = 0;   // current char position in addString
-    int aIndex = 0; // index of next Escape sequence
-    int mIndex = 0; // index of "m" terminating Escape sequence
-    String tmpString = "";
-    boolean stillSearching = true; // true until no more Escape sequences
+    int aIndex; // index of next Escape sequence
+    int mIndex; // index of "m" terminating Escape sequence
+    String tmpString;
+    boolean stillSearching; // true until no more Escape sequences
     String addString = remaining + s;
     remaining = "";
 
@@ -77,7 +76,7 @@ public class ColoredPopup extends JTextPane {
       while (stillSearching) {
         mIndex = addString.indexOf("m", aPos); // find the end of the escape sequence
         if (mIndex < 0) { // the buffer ends halfway through the ansi string!
-          remaining = addString.substring(aPos, addString.length());
+          remaining = addString.substring(aPos);
           stillSearching = false;
           continue;
         } else {
@@ -90,7 +89,7 @@ public class ColoredPopup extends JTextPane {
         aIndex = addString.indexOf("\u001B", aPos);
 
         if (aIndex == -1) { // if that was the last sequence of the input, send remaining text
-          tmpString = addString.substring(aPos, addString.length());
+          tmpString = addString.substring(aPos);
           this.append(colorCurrent, tmpString);
           stillSearching = false;
           continue; // jump out of loop early, as the whole string has been sent now
@@ -106,58 +105,46 @@ public class ColoredPopup extends JTextPane {
   }
 
   private Color getANSIColor(String ANSIColor) {
-    if (ANSIColor.equals("\u001B[30m")) {
-      return D_Black;
-    } else if (ANSIColor.equals("\u001B[31m")) {
-      return D_Red;
-    } else if (ANSIColor.equals("\u001B[32m")) {
-      return D_Green;
-    } else if (ANSIColor.equals("\u001B[33m")) {
-      return D_Yellow;
-    } else if (ANSIColor.equals("\u001B[34m")) {
-      return D_Blue;
-    } else if (ANSIColor.equals("\u001B[35m")) {
-      return D_Magenta;
-    } else if (ANSIColor.equals("\u001B[36m")) {
-      return D_Cyan;
-    } else if (ANSIColor.equals("\u001B[37m")) {
-      return D_White;
-    } else if (ANSIColor.equals("\u001B[0;30m")) {
-      return D_Black;
-    } else if (ANSIColor.equals("\u001B[0;31m")) {
-      return D_Red;
-    } else if (ANSIColor.equals("\u001B[0;32m")) {
-      return D_Green;
-    } else if (ANSIColor.equals("\u001B[0;33m")) {
-      return D_Yellow;
-    } else if (ANSIColor.equals("\u001B[0;34m")) {
-      return D_Blue;
-    } else if (ANSIColor.equals("\u001B[0;35m")) {
-      return D_Magenta;
-    } else if (ANSIColor.equals("\u001B[0;36m")) {
-      return D_Cyan;
-    } else if (ANSIColor.equals("\u001B[0;37m")) {
-      return D_White;
-    } else if (ANSIColor.equals("\u001B[1;30m")) {
-      return B_Black;
-    } else if (ANSIColor.equals("\u001B[1;31m")) {
-      return B_Red;
-    } else if (ANSIColor.equals("\u001B[1;32m")) {
-      return B_Green;
-    } else if (ANSIColor.equals("\u001B[1;33m")) {
-      return B_Yellow;
-    } else if (ANSIColor.equals("\u001B[1;34m")) {
-      return B_Blue;
-    } else if (ANSIColor.equals("\u001B[1;35m")) {
-      return B_Magenta;
-    } else if (ANSIColor.equals("\u001B[1;36m")) {
-      return B_Cyan;
-    } else if (ANSIColor.equals("\u001B[1;37m")) {
-      return B_Black;
-    } else if (ANSIColor.equals("\u001B[0m")) {
-      return cReset;
-    } else {
-      return B_Black;
+    switch (ANSIColor) {
+      case "\u001B[30m":
+      case "\u001B[0;30m":
+      case "\u001B[37m":
+      case "\u001B[0;37m":
+        return D_Black;
+      case "\u001B[31m":
+      case "\u001B[0;31m":
+        return D_Red;
+      case "\u001B[32m":
+      case "\u001B[0;32m":
+        return D_Green;
+      case "\u001B[33m":
+      case "\u001B[0;33m":
+        return D_Yellow;
+      case "\u001B[34m":
+      case "\u001B[0;34m":
+        return D_Blue;
+      case "\u001B[35m":
+      case "\u001B[0;35m":
+        return D_Magenta;
+      case "\u001B[36m":
+      case "\u001B[0;36m":
+        return D_Cyan;
+      case "\u001B[1;31m":
+        return B_Red;
+      case "\u001B[1;32m":
+        return B_Green;
+      case "\u001B[1;33m":
+        return B_Yellow;
+      case "\u001B[1;34m":
+        return B_Blue;
+      case "\u001B[1;35m":
+        return B_Magenta;
+      case "\u001B[1;36m":
+        return B_Cyan;
+      case "\u001B[0m":
+        return cReset;
+      default:
+        return B_Black;
     }
   }
 }
